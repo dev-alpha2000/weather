434997ac192997b711733c70fb12402d8a8eef64
Abhishek <abhishek18022001@gmail.com>
2023-05-20 13:39:04 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Todo from "./components/todoreact/todo";
+import Temperature from "./components/weather/Temperature.js";
+
+const App = () => {
+  return (
+    <>
+      <Temperature />
+    </>
+  );
+};
+
+export default App;
